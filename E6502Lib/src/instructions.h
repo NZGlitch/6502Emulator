4061e163aa623cb3b09550a6c570b01b61329d23
Chris Noldus <chris@noldus.co.nz>
2023-08-22 11:48:05 +1200

    Basic tests, Mem, and CPU in place

---
+++ E6502Lib/src/instructions.h
@@ -0,0 +1,4 @@
+// instructions.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
--- E6502Test/src/gtest_main.cpp
+++ E6502Test/src/gtest_main.cpp
@@ -2,7 +2,8 @@
 #define TEST_OR_EMULATE 1
 #if TEST_OR_EMULATE
 
-#include "gtest/gtest.h"
+//#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #if GTEST_OS_ESP8266 || GTEST_OS_ESP32
 #if GTEST_OS_ESP8266
 extern "C" {
@@ -22,6 +23,7 @@ extern "C" {
 GTEST_API_ int main(int argc, char** argv) {
 	printf("Running main() from %s\n", __FILE__);
 	testing::InitGoogleTest(&argc, argv);
+	testing::InitGoogleMock(&argc, argv);
 	return RUN_ALL_TESTS();
 }
 #endif
@@ -30,8 +32,10 @@ GTEST_API_ int main(int argc, char** argv) {
 
 #include "main.h"
 int main() {
+	Mem mem;
 	CPU cpu;
-	cpu.reset();
+	cpu.reset(mem);
+	u8 cyclesExecuted = cpu.execute(1, mem);
 	return 0;
 }
 
