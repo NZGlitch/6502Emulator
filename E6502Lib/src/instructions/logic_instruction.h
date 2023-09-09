59e50aaebd30070e30000107befc4cffb11b5dca
Chris Noldus <chris@noldus.co.nz>
2023-09-09 18:08:13 +1200

    Implemented logic ABS instructions

--- E6502Lib/src/instructions/logic_instruction.h
+++ E6502Lib/src/instructions/logic_instruction.h
@@ -9,10 +9,10 @@ namespace E6502 {
 	public:
 
 		// Operations mapped to op field value (Opfield is bits 7,6,5,1,0 of the OpCode)
-		constexpr static Byte OP_AND = 0x5;
-		constexpr static Byte OP_BIT = 0x0;
 		constexpr static Byte OP_EOR = 0x9;
+		constexpr static Byte OP_AND = 0x5;
 		constexpr static Byte OP_ORA = 0x1;
+		constexpr static Byte OP_BIT = 0x4;
 
 		static Byte AND(Byte a, Byte b) { return a & b; }
 		static Byte EOR(Byte a, Byte b) { return a ^ b; }
@@ -21,36 +21,37 @@ namespace E6502 {
 		/** Handles execution of all logical instructions */
 		static void logicHandler(CPU* cpu, u8& cycles, Byte opCode);
 
-		/* Helper method actually performs the required operation */
-		//static void performOp(CPU* cpu, u8& cycles, Byte op, Byte& value, Byte& carry);
-
 		/** Called to add Logic Instruction handlers to the emulator */
 		static void addHandlers(InstructionHandler* handlers[]);
 	};
 
-	/** AND Instruction Definitions Field A: 001, Field C: 01 */
-	constexpr static InstructionHandler INS_AND_IMM = { 0x29, true, "AND - 'AND' Memory with Accumulator [Immediate]", LogicInstruction::logicHandler };
-
-	/** BIT Instruction Definitions Field A: ???, Field C: ?? */
-	
 	/** EOR Instruction Definitions Field A: 010, Field C: 01 */
 	constexpr static InstructionHandler INS_EOR_IMM = { 0x49, true, "EOR - 'Exclusive Or' Memory with Accumulator [Immediate]", LogicInstruction::logicHandler };
-	
+	constexpr static InstructionHandler INS_EOR_ABS = { 0x4D, true, "EOR - 'Exclusive Or' Memory with Accumulator [Absolute]", LogicInstruction::logicHandler };
+
+	/** AND Instruction Definitions Field A: 001, Field C: 01 */
+	constexpr static InstructionHandler INS_AND_IMM = { 0x29, true, "AND - 'AND' Memory with Accumulator [Immediate]", LogicInstruction::logicHandler };
+	constexpr static InstructionHandler INS_AND_ABS = { 0x2D, true, "AND - 'AND' Memory with Accumulator [Absolute]", LogicInstruction::logicHandler };
+
 	/** ORA Instruction Definitions Field A: 000, Field C: 01  */
 	constexpr static InstructionHandler INS_ORA_IMM = { 0x09, true, "ORA - 'OR' Memory with Accumulator [Immediate]", LogicInstruction::logicHandler };
+	constexpr static InstructionHandler INS_ORA_ABS = { 0x0D, true, "ORA - 'OR' Memory with Accumulator [Absolute]", LogicInstruction::logicHandler };
+
+	/** BIT Instruction Definitions Field A: 001, Field C: 00 */
+	constexpr static InstructionHandler INS_BIT_ABS = { 0x2C, true, "BIT - Test Bits in Memory with Accumulator [Absolute]", LogicInstruction::logicHandler };
 	
 	// Array of all logic instructions
 	static constexpr InstructionHandler LOGIC_INSTRUCTIONS[] = {
-		/** AND Instruction Definitions */
-		INS_AND_IMM,
-
-		/** BIT Instruction Definitions */
-		
-
 		/** EOR Instruction Definitions */
-		INS_EOR_IMM,
+		INS_EOR_IMM, INS_EOR_ABS,
+
+		/** AND Instruction Definitions */
+		INS_AND_IMM, INS_AND_ABS,
 
 		/** ORA Instruction Definitions */
-		INS_ORA_IMM,		
+		INS_ORA_IMM, INS_ORA_ABS,
+
+		/** BIT Instruction Definitions */
+		INS_BIT_ABS,
 	};
 }
--- E6502Test/src/instructions/logic_instruction.cpp
+++ E6502Test/src/instructions/logic_instruction.cpp
@@ -3,10 +3,25 @@
 #include "logic_instruction.h"
 
 namespace E6502 {
+
+	struct LogicFixture {
+		Byte operandA;
+		Byte operandB;
+		Byte result;
+	};
+
 	class TestLogicInstruction : public TestInstruction {
 	private:	
+		/* Reused code for test setup for helpers below */
+		LogicFixture prepareTest(InstructionHandler instruction, Byte(*op)(Byte a, Byte b)) {
+			Byte a = rand();
+			Byte b = rand();
+			Byte expectedResult = op(a, b);
+			(*memory)[programSpace] = instruction.opcode;
+			return  { a,b,expectedResult };
+		}
 
-		/* Reused code for finishing tests from helpers below */
+		/* Reused code for finishing tests for helpers below */
 		void finishTest(Byte expectedResult, u8 expectedCycles, u8 actualCycles) {
 			// Then
 			EXPECT_EQ(expectedResult, state->A);
@@ -18,22 +33,40 @@ namespace E6502 {
 		/* Helper method for testing operations using immediate addressing mode */
 		void testImmOp(InstructionHandler instruction, Byte(*op)(Byte a, Byte b), Byte expectedCycles) {
 			// Given
-			Byte a = rand();
-			Byte b = rand();
-			Byte expectedResult = op(a, b);
-			state->A = a;
-			(*memory)[programSpace] = instruction.opcode;
-			(*memory)[programSpace + 1] = b;
+			LogicFixture fixtures = prepareTest(instruction, op);
+			state->A = fixtures.operandA;
+			(*memory)[programSpace + 1] = fixtures.operandB;
 
 			// When:
 			u8 cycles = cpu->execute(1);
 
 			// Then:
-			finishTest(expectedResult, expectedCycles, cycles);
+			finishTest(fixtures.result, expectedCycles, cycles);
 		}
 
 		/* Helper method for testing operations using absolute addressing mode */
-		void testAbsOp(InstructionHandler instruction) {}
+		void testAbsOp(InstructionHandler instruction, Byte(*op)(Byte a, Byte b), Byte expectedCycles) {
+			// Given
+			LogicFixture fixtures = prepareTest(instruction, op);
+			state->A = fixtures.operandA;
+			(*memory)[programSpace] = instruction.opcode;
+			(*memory)[programSpace + 1] = dataSpace & 0x00FF;
+			(*memory)[programSpace + 2] = dataSpace >> 8;
+			(*memory)[dataSpace] = fixtures.operandB;
+
+			// When:
+			u8 cycles = cpu->execute(1);
+
+			// Then:
+			if (instruction == INS_BIT_ABS) {	// BIT instructions dont sace result
+				EXPECT_EQ(state->A, fixtures.operandA);
+				EXPECT_EQ((*memory)[dataSpace], fixtures.operandB);
+				testAndResetStatusFlags(fixtures.result);
+			}
+			else {
+				finishTest(fixtures.result, expectedCycles, cycles);
+			}
+		}
 
 		/* Helper method for testing operations using absolute addressing-x mode */
 		void testAbsXOp(InstructionHandler) {}
@@ -70,30 +103,33 @@ namespace E6502 {
 	TEST_F(TestLogicInstruction, TestLogicHandlers) {
 
 		std::vector<InstructionMap> instructions = {
-			// AND Instructions
-			{INS_AND_IMM, 0x29},
-
-			// BIT Instructions
-			
-
 			// EOR Instructions
-			{INS_EOR_IMM, 0x49},
+			{INS_EOR_IMM, 0x49}, {INS_EOR_ABS, 0x4D},
+
+			// AND Instructions
+			{INS_AND_IMM, 0x29}, {INS_AND_ABS, 0x2D},
 
 			// ORA Instructions
-			{INS_ORA_IMM, 0x09},
+			{INS_ORA_IMM, 0x09}, {INS_ORA_ABS, 0x0D},
+
+			// BIT Instructions
+			{INS_BIT_ABS, 0x2C},
 		};
 		testInstructionDef(instructions, LogicInstruction::addHandlers);
 	}
 
-	/* Test AND execution */
-	// IMM
-	TEST_F(TestLogicInstruction, TestANDImmediate) { testImmOp(INS_AND_IMM, LogicInstruction::AND, 2);  }
-
 	/* Test EOR execution */
-	// IMM
 	TEST_F(TestLogicInstruction, TestEORImmediate) { testImmOp(INS_EOR_IMM, LogicInstruction::EOR, 2); }
+	TEST_F(TestLogicInstruction, TestEORbAsolute) { testAbsOp(INS_EOR_ABS, LogicInstruction::EOR, 4); }
 
+	/* Test AND execution */
+	TEST_F(TestLogicInstruction, TestANDImmediate) { testImmOp(INS_AND_IMM, LogicInstruction::AND, 2);  }
+	TEST_F(TestLogicInstruction, TestANDAbsolute) { testAbsOp(INS_AND_ABS, LogicInstruction::AND, 4); }
+	
 	/* Test ORA execution */
-	// IMM
 	TEST_F(TestLogicInstruction, TestORAImmediate) { testImmOp(INS_ORA_IMM, LogicInstruction::ORA, 2); }
+	TEST_F(TestLogicInstruction, TestORAAbsolute) { testAbsOp(INS_ORA_ABS, LogicInstruction::ORA, 4); }
+
+	/* Test BIT execution */
+	TEST_F(TestLogicInstruction, TestBITAbsolute) { testAbsOp(INS_BIT_ABS, LogicInstruction::AND, 4); }
 }
